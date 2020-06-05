26fbcfcadfa067282a3df20116d1e69f6a9f1fbf
thealekid <chukorakwusi@gmail.com>
2020-06-05 18:20:14 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const sentence = document.querySelector("#text");
+    sentence.innerHTML = "This is really cool!";
+});
